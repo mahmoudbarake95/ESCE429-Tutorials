ea9c9479dea7941d46307e86d5327ffe83245d11
Mahmoud Barake <mahmoudbarake95@gmail.com>
2018-09-17 16:07:43 -0400

    initial commit

---
+++ src/main/java/tutorial2/tutorial2_artifact/App.java
@@ -0,0 +1,13 @@
+package tutorial2.tutorial2_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
